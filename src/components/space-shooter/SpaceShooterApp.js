b5bc4cd2f729edff6fef18d26f83961d83bfe6df
BenedictRasmussen <benedictrasmussen@gmail.com>
2018-06-18 22:44:36 -0500

    WIP: Rolling back previous change, turns out that just made it worse...

--- src/components/space-shooter/SpaceShooterApp.js
+++ src/components/space-shooter/SpaceShooterApp.js
@@ -4,17 +4,9 @@ import '../../resources/css/space-shooter.css';
 
 const SpaceShooterApp = () => (
   <div className="webgl-content">
-    <Unity src="%PUBLIC_URL%/space-shooter/Build/SpaceShooter.json"
-      loader="%PUBLIC_URL%/space-shooter/Build/UnityLoader.js" />
+    <Unity src="public/space-shooter/Build/SpaceShooter.json"
+      loader="public/space-shooter/Build/UnityLoader.js" />
   </div>
 );
 
 export default SpaceShooterApp;
-
-/*
-  <div className="webgl-logo" />
-  <div className="footer">
-  <div className="fullscreen" onClick="gameInstance.SetFullscreen(1)" />
-  <div className="title">SpaceShooter</div>
-</div>
-*/
