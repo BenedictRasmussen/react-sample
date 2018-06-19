4b9f84fab3aeb86169610660ae1ecab1c774e42c
BenedictRasmussen <benedictrasmussen@gmail.com>
2018-06-18 22:29:06 -0500

    Read that unity-webgl issues may be due to local server, attempting heroku push

---
+++ src/components/space-shooter/SpaceShooterApp.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Unity from 'react-unity-webgl';
+import '../../resources/css/space-shooter.css';
+
+const SpaceShooterApp = () => (
+  <div className="webgl-content">
+    <Unity src="./space-shooter/Build/SpaceShooter.json"
+      loader="./space-shooter/Build/UnityLoader.js" />
+  </div>
+);
+
+export default SpaceShooterApp;
+
+/*
+  <div className="webgl-logo" />
+  <div className="footer">
+  <div className="fullscreen" onClick="gameInstance.SetFullscreen(1)" />
+  <div className="title">SpaceShooter</div>
+</div>
+*/
