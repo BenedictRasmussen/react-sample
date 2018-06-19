fb1344d43ada1df5f7c6028d2e24d233225a3af6
BenedictRasmussen <benedictrasmussen@gmail.com>
2018-06-18 22:58:18 -0500

    Moved unity back to its own folder

--- src/components/space-shooter/SpaceShooterApp.js
+++ src/components/space-shooter/SpaceShooterApp.js
@@ -4,8 +4,8 @@ import '../../resources/css/space-shooter.css';
 
 const SpaceShooterApp = () => (
   <div className="webgl-content">
-    <Unity src="/Build/SpaceShooter.json"
-      loader="/Build/UnityLoader.js" />
+    <Unity src="/space-shooter/Build/SpaceShooter.json"
+      loader="/space-shooter/Build/UnityLoader.js" />
   </div>
 );
 
